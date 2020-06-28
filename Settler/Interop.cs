b33d3949af5fc812f2c83d3ce5d6e8f0f8f60c71
mf <mf@mail.de>
2020-06-28 23:01:22 +0200

    init

---
+++ Settler/Interop.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Settler
+{
+    class Interop
+    {
+        // reference https://stackoverflow.com/questions/10355286/programmatically-mouse-click-in-another-window
+
+        [DllImport("user32.dll")]
+        static extern bool ClientToScreen(IntPtr hWnd, ref Point lpPoint);
+
+        [DllImport("user32.dll")]
+        internal static extern uint SendInput(uint nInputs, [MarshalAs(UnmanagedType.LPArray), In] INPUT[] pInputs, int cbSize);
+
+#pragma warning disable 649
+        internal struct INPUT
+        {
+            public UInt32 Type;
+            public MOUSEKEYBDHARDWAREINPUT Data;
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        internal struct MOUSEKEYBDHARDWAREINPUT
+        {
+            [FieldOffset(0)]
+            public MOUSEINPUT Mouse;
+        }
+
+        internal struct MOUSEINPUT
+        {
+            public Int32 X;
+            public Int32 Y;
+            public UInt32 MouseData;
+            public UInt32 Flags;
+            public UInt32 Time;
+            public IntPtr ExtraInfo;
+        }
+
+#pragma warning restore 649
+
+        public static void ClickOnPoint(IntPtr wndHandle, Point clientPoint)
+        {
+            var oldPos = Cursor.Position;
+
+            /// get screen coordinates
+            ClientToScreen(wndHandle, ref clientPoint);
+
+            /// set cursor on coords, and press mouse
+            Cursor.Position = new Point(clientPoint.X, clientPoint.Y);
+
+            var inputMouseDown = new INPUT();
+            inputMouseDown.Type = 0; /// input type mouse
+            inputMouseDown.Data.Mouse.Flags = 0x0002; /// left button down
+
+            var inputMouseUp = new INPUT();
+            inputMouseUp.Type = 0; /// input type mouse
+            inputMouseUp.Data.Mouse.Flags = 0x0004; /// left button up
+
+            var inputs = new INPUT[] { inputMouseDown, inputMouseUp };
+            SendInput((uint)inputs.Length, inputs, Marshal.SizeOf(typeof(INPUT)));
+
+            /// return mouse 
+            Cursor.Position = oldPos;
+        }
+
+        // reference https://stackoverflow.com/questions/9668872/how-to-get-windows-position
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr FindWindow(string strClassName, string strWindowName);
+
+        [DllImport("user32.dll")]
+        public static extern bool GetWindowRect(IntPtr hwnd, ref Rect rectangle);
+
+        public struct Rect
+        {
+            public int Left { get; set; }
+            public int Top { get; set; }
+            public int Right { get; set; }
+            public int Bottom { get; set; }
+        }
+
+        // reference https://stackoverflow.com/questions/1190423/using-setwindowpos-in-c-sharp-to-move-windows-around 
+        [DllImport("user32.dll", EntryPoint = "SetWindowPos")]
+        public static extern IntPtr SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int Y, int cx, int cy, int wFlags);
+
+        public static IntPtr HandleFromTitle(String title)
+        {
+            Process[] list = Process.GetProcessesByName(title);
+            Process p2 = list[0];
+            IntPtr handle = p2.MainWindowHandle;
+            return handle;
+        }
+
+        public static Point WindowOffsetPoint(IntPtr handle, int x, int y)
+        {
+            Rect rect = new Rect();
+            Interop.GetWindowRect(handle, ref rect);
+            Point pos = new Point(rect.Left + x, rect.Top + y);
+            return pos;
+        }
+    }
+}
---
+++ Settler/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.Drawing;
+using System.Threading;
+
+namespace Settler
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // TODO BEWARE handle injection attacks
+            String path = ConfigurationManager.AppSettings.Get("Path");
+            String title = ConfigurationManager.AppSettings.Get("Title");
+            int delay = Int32.Parse(ConfigurationManager.AppSettings.Get("Delay"));
+            int width = Int32.Parse(ConfigurationManager.AppSettings.Get("WindowWidth"));
+            int height = Int32.Parse(ConfigurationManager.AppSettings.Get("WindowHeight"));
+            int x = Int32.Parse(ConfigurationManager.AppSettings.Get("OffsetX"));
+            int y = Int32.Parse(ConfigurationManager.AppSettings.Get("OffsetY"));
+
+            const short SWP_NOZORDER = 0X4;
+            const int SWP_SHOWWINDOW = 0x0040;
+
+            Process.Start(path);
+            Write("Start " + path);
+            Write("Waiting " + delay + " milliseconds");
+            Thread.Sleep(delay);
+            IntPtr handle = Interop.HandleFromTitle(title);
+            Write("Fetching handle " + handle);
+            Interop.SetWindowPos(handle, 0, 0, 0, width, height, SWP_NOZORDER | SWP_SHOWWINDOW);
+            Point pos = Interop.WindowOffsetPoint(handle, x, y);
+            Write("Position click " + pos.X + "," + pos.Y);
+            Interop.ClickOnPoint(handle, pos);
+            Console.WriteLine("Should have started");
+            Thread.Sleep(3000); // wait 3 sec before exiting
+            //Console.ReadKey(); // uncomment if window should stay open
+        }
+
+        static void Write(string txt)
+        {
+            Console.WriteLine(txt);
+        }
+    }
+}
